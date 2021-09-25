562813bfd75901825cd52dbdf81a92003d83f9bf
TheScorpionn <djvpanda@gmail.com>
2021-09-25 14:55:52 -0300

    ✨ Implementando o while
    Colocando a função while e escrevendo o que deverá ser repetido para o funcionamento do jogo

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import random
 
 cont = 1
 nivel = 1
+while (cont <= 7 and nivel < 9):
+    print("Você está na sala: {0}." .format(nivel))
+    print("Escolha seu caminho: ")
+    print("[1] - Caminho vermelho")
+    print("[2] - Caminho preto")
+    escolha = int(input())
+    cont = cont + 1
+    nivel = nivel + escolha
