c41fd2dd169cca0c30ab248c4db1cefa6c2095eb
TheScorpionn <djvpanda@gmail.com>
2021-09-25 14:59:49 -0300

    ✨ Implementando as decisões
    Escrevendo as condições para a sala 6 e para o portal da sala 8

--- main.py
+++ main.py
@@ -10,3 +10,10 @@ while (cont <= 7 and nivel < 9):
     escolha = int(input())
     cont = cont + 1
     nivel = nivel + escolha
+    if(nivel == 10):
+        print("Você entrou em um portal dimensional que te levará a uma sala aleatória de 1 a 5")
+        nivel = random.randrange(1, 6)
+    elif(nivel == 6):
+        print("Você está na sala: {0}." .format(nivel))
+        print("Você será redirecionado para a sala: 8 ")
+        nivel = 8
